3f5099129cf01abb9f1d253f06866829c006785b
wlissis <wlissisdev@outlook.com>
2022-05-08 00:18:14 -0300

    final commit

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,7 +1,7 @@
 import { CloseButton } from "../../CloseButton";
 
 interface FeedbackSuccessStepProps {
-    onFeedbackRestartRequest: ()=>void;
+  onFeedbackRestartRequest: () => void;
 }
 
 export function FeedbackSuccessStep(props: FeedbackSuccessStepProps) {
@@ -32,11 +32,11 @@ export function FeedbackSuccessStep(props: FeedbackSuccessStepProps) {
         <span className="text-xl mt-2">Agradecemos o feedback!</span>
 
         <button
-            type="button"
-            onClick={props.onFeedbackRestartRequest}
-            className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500  disabled:opacity-50 disabled:hover:bg-brand-500"
+          type="button"
+          onClick={props.onFeedbackRestartRequest}
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500  disabled:opacity-50 disabled:hover:bg-brand-500"
         >
-            Quero enviar outro
+          Quero enviar outro
         </button>
       </div>
     </>
